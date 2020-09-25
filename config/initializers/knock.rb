cbce9619ca567f24b1b0b0e8dc16f7906d1a004d
benl798 <ben.l7@hotmail.com>
2020-09-25 10:48:15 +1000

    Deploy to Heroku

--- config/initializers/knock.rb
+++ config/initializers/knock.rb
@@ -4,8 +4,13 @@ Knock.setup do |config|
 
   config.token_signature_algorithm = 'HS256'
 
-  config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  # config.token_secret_signature_key = -> { Rails.application.secrets.secret_key_base }
+  # Rails.application.credentials.fetch(:secret_key_base)
+  config.token_secret_signature_key = -> { Rails.application.credentials.read }
+  # config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
+
 
   config.not_found_exception_class_name = 'ActiveRecord::RecordNotFound'
-  
+
+
 end
